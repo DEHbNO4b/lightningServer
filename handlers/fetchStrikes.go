a5f813a117a250cd81d8229825675f7492e6e59f
DEHbNO4b <85077586+DEHbNO4b@users.noreply.github.com>
2023-07-14 15:47:42 +0000

    modified fetchStrikes handler for 10 min

--- handlers/fetchStrikes.go
+++ handlers/fetchStrikes.go
@@ -3,8 +3,8 @@ package handlers
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -21,17 +21,15 @@ func NewFetchStrikes(db *sql.DB) *FetchStrikes {
 	return &FetchStrikes{db}
 }
 func (f FetchStrikes) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+
 	requestedDay := r.URL.Query().Get("day")
-	day, err := time.Parse("2006.01.02_MST", requestedDay)
+	day, err := time.Parse("2006.01.02_15:04_MST", requestedDay)
 	if err != nil {
 		http.Error(rw, "Unable purse requested day", http.StatusInternalServerError)
 	}
-	//day.Add(-3)
-	day2 := day.AddDate(0, 0, 1)
-	fmt.Println("day;", day)
-	fmt.Println("date2;", day2)
-
-	rows, err := f.DB.Query(`SELECT longitude,latitude,cluster FROM strikes where time between $1 and $2`, day, day2)
+	// day2 := day.AddDate(0, 0, 1)
+	time2 := day.Add(10 * time.Minute)
+	rows, err := f.DB.Query(`SELECT longitude,latitude,cluster FROM enstrikes where time between $1 and $2`, day, time2)
 	if err != nil {
 		http.Error(rw, "Unable to connect DB", http.StatusInternalServerError)
 	}
@@ -40,19 +38,30 @@ func (f FetchStrikes) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	var cl string
 	var data []strike
 	for rows.Next() {
-		if err = rows.Scan(&long, &lat, &cl); err != nil {
-			http.Error(rw, "Unable to connect DB", http.StatusInternalServerError)
+		if err := rows.Scan(&long, &lat, &cl); err != nil {
+			continue
+			// http.Error(rw, "Unable to connect DB", http.StatusInternalServerError)
+			// return
 		}
 		s := strike{Longitude: long, Latitude: lat, Cluster: cl}
+		if len(s.Cluster) > 2 {
+			c := s.Cluster[len(s.Cluster)-2:]
+			c, _ = strings.CutPrefix(c, ":")
+			s.Cluster = c
+		}
 
 		data = append(data, s)
 	}
+
 	d, err := json.Marshal(data)
+
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
+		return
 	}
 	rw.Header().Set("Access-Control-Allow-Origin", "*")
 	rw.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, PATCH")
 	rw.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	rw.WriteHeader(http.StatusOK)
 	rw.Write(d)
 }
